07e02b08c7acadab1506a9ba9b600e98f9f873d4
joseph.codes <j.williamson@gambler-labs.com>
2019-11-18 05:50:13 +0000

    Support for Insert querys

--- DBManager.php
+++ DBManager.php
@@ -3,17 +3,37 @@ class DBManager{
 	protected $credentials = array(
 		"host"	=> "localhost",
 		"user"	=> "root",
-		"pass"	=> "password",
-		"db"	=> "test"
+		"pass"	=> "",
+		"db"	=> "testing"
 	);
 
-	public static function DBManager(){
+	public function DBManager(){
 		$this->ConnectToServer();
 	}
 
 	protected function ConnectToServer(){
 		mysql_connect($this->credentials["host"],$this->credentials["user"],$this->credentials["pass"]);
 		mysql_select_db($this->credentials["db"]);
+	}
+
+	function Insert($table_name,$data){
+		$parameters = "";
+
+		$len =  count($data);
+		$i = 0;
+
+		foreach($data as $key => $value){
+			//If the next iteration is the last iteration
+			//then we don't need to add a comma at the end
+			if(++$i === $len){
+				$parameters .= $key . "='$value'";					
+			}else{
+				$parameters .= $key . "='$value',";
+			}
+		}
+
+		$query = "INSERT INTO $table_name SET $parameters";
+		mysql_query($query) or die(mysql_error());
 	}
 }
 ?>
